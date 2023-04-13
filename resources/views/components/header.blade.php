80eb2b27c6cf443d2d3dccc1fbbb4956416849cb
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-04-13 12:16:24 +0200

    Add books view

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,10 +4,10 @@
                 {{-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" /> --}}
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Borrow A Shiver</span>
             </a>
-                <a href="#" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Books</a>
-                <a href="#" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Reserve</a>
-                <a href="#" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Contact</a>
-                <a href="#" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Login</a>
-                <a href="#" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Logout</a>
+                <a href="{{ route('books') }}" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Books</a>
+                <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Reserve</a>
+                <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Contact</a>
+                <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+                <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Logout</a>
     </nav>
 </header>

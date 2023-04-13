c3575962e980df4f8df2d7e70d41d5a60977cb2d
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-04-13 12:34:16 +0200

    Fix navigation links

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -2,15 +2,34 @@
 
 @section('content')
 
-<div>
-    @foreach ($books as $book)
+<div class="bg-gray-900 text-white">
+    <div class="container mx-auto mt-10">
+        <h1 class="text-3xl font-bold mb-5">Books</h1>
+        <div class="overflow-x-auto">
+            <table class="table-auto border-collapse border border-gray-700 w-full">
+                <thead>
+                    <tr>
+                        <th class="px-4 py-2 text-left">Author</th>
+                        <th class="px-4 py-2 text-left">Title</th>
+                        <th class="px-4 py-2 text-left">Language</th>
+                        <th class="px-4 py-2 text-left">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($books as $book)
 
-<div>
-    <p>Author: {{$book->author}}</p>
-    <p>Title: {{$book->title}}</p>
-</div>
-        
-    @endforeach
+                    <tr>
+                        <td class="border border-gray-700 px-4 py-2 text-left">{{$book->author}}</td>
+                        <td class="border border-gray-700 px-4 py-2 text-left">{{$book->title}}</td>
+                        <td class="border border-gray-700 px-4 py-2 text-left">{{$book->language}}</td>
+                        <td class="border border-gray-700 px-4 py-2 text-left">{{$book->status}}</td>
+                    </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 @endsection
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,13 +1,14 @@
 <header>
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
-            <a href="#" class="flex items-center">
+            <a href="{{ route('homepage') }}" class="flex items-center">
                 {{-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" /> --}}
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Borrow A Shiver</span>
             </a>
-                <a href="{{ route('books') }}" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Books</a>
-                <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Reserve</a>
-                <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Contact</a>
-                <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Login</a>
-                <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Logout</a>
+            <a href="{{ route('homepage') }}" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Home</a>
+            <a href="{{ route('books') }}" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Books</a>
+            <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Reserve</a>
+            <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Contact</a>
+            <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+            <a href="" class="text-sm text-blue-600 dark:text-blue-500 hover:underline">Logout</a>
     </nav>
 </header>

ea7dbc0f515b1d943c286b49956d9ff07f2be8e9
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-04-17 10:46:57 +0200

    Create comment blade component

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -2,13 +2,19 @@
 
 @section('content')
 
-<div>
+<section>
     <h1>{{$book->title}}</h1>
     {{-- TODO: allow for more authors --}}
     <p><a href="/{{$book->author->slug}}">{{$book->author->authorName()}}</a></p>
     <p>{{$book->language->name}}</p>
     <p>{{$book->description}}</p>
     <p>{{$book->status}}</p>
-</div>
+</section>
+
+<h2 class="font-bold text-xl">Comments</h2>
+
+<section>
+<x-post-comment />
+</section>
 
 @endsection
---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,16 @@
+<article class="flex">
+    <div>
+        <img src='' alt='avatar image'>
+    </div>
+    <div>
+        <header>
+        <h3 class='font-bold'>Smithson</h3>
+        <p class="text-xs">Posted
+            <time>8 months ago</time>
+        </p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod error consectetur provident quia, adipisci atque amet sed ea illum, suscipit doloremque, at id nam beatae eius excepturi aperiam dolore unde?
+        </p>
+    </div>
+</article>

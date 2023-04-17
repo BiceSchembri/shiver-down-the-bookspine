2a7bc7b30a782816b37c69144653d6cdb6f699bd
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-04-17 11:33:34 +0200

    Add cascade on delete for comments / users

--- database/migrations/2023_04_17_075409_create_comments_table.php
+++ database/migrations/2023_04_17_075409_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('book_id')->references('id')->on('books')->onDelete('cascade');
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->text('body');
             $table->timestamps();
         });

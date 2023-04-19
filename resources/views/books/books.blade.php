2c6206ae9fbefbd1410722e9112671bf6dc24c27
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-04-19 10:57:02 +0200

    Style books view

--- resources/views/books/books.blade.php
+++ resources/views/books/books.blade.php
@@ -6,24 +6,24 @@
 
 <div class="bg-gray-900 text-white">
     <div class="container mx-auto mt-10">
-        <h1 class="text-3xl font-bold mb-5">Books</h1>
+        <h1 class="text-3xl font-bold mb-5 p-2">Books</h1>
         <div class="overflow-x-auto">
             <table class="table-auto border-collapse border border-gray-700 w-full">
                 <thead>
                     <tr>
-                        <th class="px-4 py-2 text-left">Title</th>
-                        <th class="px-4 py-2 text-left">Author</th>
-                        <th class="px-4 py-2 text-left">Language</th>
-                        <th class="px-4 py-2 text-left">Status</th>
+                        <th class="px-4 py-2 text-left uppercase">Title</th>
+                        <th class="px-4 py-2 text-left uppercase">Author</th>
+                        <th class="px-4 py-2 text-left uppercase">Language</th>
+                        <th class="px-4 py-2 text-left uppercase">Status</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($books as $book)
 
                     <tr>
-                        <td class="border border-gray-700 px-4 py-2 text-left"><a href="/books/detail/{{$book->slug}}" class="text-green-300 hover:text-green-400">{{$book->title}}</a></td>
-                        <td class="border border-gray-700 px-4 py-2 text-left"><a href="/authors/detail/{{$book->author->slug}}" class="text-purple-300 hover:text-purple-400">{{$book->author->authorName()}}</a></td>
-                        <td class="border border-gray-700 px-4 py-2 text-left">{{$book->language->name}}</td>
+                        <td class="border border-gray-700 px-4 py-2 text-left hover:bg-purple-900"><a href="/books/detail/{{$book->slug}}" class="text-purple-200">{{$book->title}}</a></td>
+                        <td class="border border-gray-700 px-4 py-2 text-left hover:bg-purple-900"><a href="/authors/detail/{{$book->author->slug}}" class="text-purple-200">{{$book->author->authorName()}}</a></td>
+                        <td class="border border-gray-700 px-4 py-2 text-left text-purple-200">{{$book->language->name}}</td>
                         <td class="border border-gray-700 px-4 py-2 text-left">
                             @if ($book->status == 'available')
                                 <span class="bg-green-300 text-gray-900 px-2 py-1 rounded-full">{{$book->status}}</span>

a4f0f913c3c6a2eea1940a835bd554a945f9e202
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-04-19 11:07:27 +0200

    Style authors view

--- resources/views/authors/authors.blade.php
+++ resources/views/authors/authors.blade.php
@@ -7,24 +7,25 @@
 
 <div class="bg-gray-900 text-white">
     <div class="container mx-auto mt-10">
-        <h1 class="text-3xl font-bold mb-5">Authors</h1>
+        <h1 class="text-3xl font-bold mb-5 p-2">Authors</h1>
         <div class="overflow-x-auto">
             <table class="table-auto border-collapse border border-gray-700 w-full">
                 <thead>
                     <tr>
-                        <th class="px-4 py-2 text-left">Last Name</th>
-                        <th class="px-4 py-2 text-left">First Name</th>
-                        <th class="px-4 py-2 text-left">Link</th>
+                        <th class="px-4 py-2 text-left uppercase">Last Name</th>
+                        <th class="px-4 py-2 text-left uppercase">First Name</th>
+                        <th class="px-4 py-2 text-left uppercase">Page</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($authors as $author)
 
-                    <tr>
-                        <td class="border border-gray-700 px-4 py-2 text-left">{{$author->lastname}}</td>                        
-                        <td class="border border-gray-700 px-4 py-2 text-left">{{$author->firstname}}</td>         
-                        <td class="border border-gray-700 px-4 py-2 text-left"><a href="/authors/detail/{{$author->slug}}">click here for more info
-                        </a></td>
+                    <tr class="border border-gray-700 hover:bg-purple-900">
+                        <td class="px-4 py-2 text-left">{{$author->lastname}}</td>                        
+                        <td class="px-4 py-2 text-left">{{$author->firstname}}</td>         
+                        <td class="px-4 py-2 text-left">
+                            <a href="/authors/detail/{{$author->slug}}" class="bg-purple-200 text-gray-900 px-2 py-1 rounded-full">read info</a>
+                        </td>
                     </tr>
 
                     @endforeach

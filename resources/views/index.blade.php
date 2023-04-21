3faf9340a56492f31020c0e050ddb97016f300e9
Beatrice Schembri <beatrice.schembri@gmail.com>
2023-04-21 23:31:42 +0200

    Add admin column to users

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 
 <div class="bg-gray-900 opacity-95 text-white text-center p-10">
     <p class="text-purple-200">Browse the books of this little library of horrors.</p>
-    <p class="text-purple-200">Leave your comments and share spooky ideas.</p>
+    <p class="text-purple-200">Leave a spooky comment if you'd like.</p>
 </div>
     
 @endsection
